cfc37ced374420430cf52498bf3d68d12482201a
Leo <leo@apotheca.io>
2023-07-15 15:13:10 -0500

    Extended Botan.Make, completed Botan.Cipher with new make-functions

--- src/Botan/Cipher.hs
+++ src/Botan/Cipher.hs
@@ -64,3 +64,194 @@ foreign import ccall unsafe "&botan_cipher_destroy" botan_cipher_destroy :: Fina
 cipherInit :: CipherName -> CipherFlags -> IO Cipher
 cipherInit = mkInit_name_flags MkCipher botan_cipher_init botan_cipher_destroy
 
+-- /**
+-- * Return the name of the cipher object
+-- */
+-- BOTAN_PUBLIC_API(2,8) int botan_cipher_name(botan_cipher_t cipher, char* name, size_t* name_len);
+foreign import ccall unsafe botan_cipher_name :: CipherPtr -> Ptr CChar -> Ptr CSize -> IO BotanErrorCode
+
+-- TODO: Unify with other -Name function which are effectively copies.
+cipherName :: Cipher -> IO CipherName
+cipherName = mkGetName withCipherPtr botan_cipher_name
+
+-- /**
+-- * Return the output length of this cipher, for a particular input length.
+-- */
+-- BOTAN_PUBLIC_API(2,8) int botan_cipher_output_length(botan_cipher_t cipher, size_t in_len, size_t* out_len);
+foreign import ccall unsafe botan_cipher_output_length :: CipherPtr -> CSize -> Ptr CSize -> IO BotanErrorCode
+
+cipherOutputLength :: Cipher -> Int -> IO Int
+cipherOutputLength = mkGetSize_csize withCipherPtr botan_cipher_output_length
+
+-- /**
+-- * Return if the specified nonce length is valid for this cipher
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_valid_nonce_length(botan_cipher_t cipher, size_t nl);
+foreign import ccall unsafe botan_cipher_valid_nonce_length :: CipherPtr -> CSize -> IO BotanErrorCode
+
+-- NOTE: Unique function form?
+cipherValidNonceLength :: Cipher -> Int -> IO Bool
+cipherValidNonceLength = mkGetBoolCode_csize withCipherPtr botan_cipher_valid_nonce_length
+
+-- /**
+-- * Get the tag length of the cipher (0 for non-AEAD modes)
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_get_tag_length(botan_cipher_t cipher, size_t* tag_size);
+foreign import ccall unsafe botan_cipher_get_tag_length :: CipherPtr -> Ptr CSize -> IO BotanErrorCode
+
+cipherGetTagLength :: Cipher -> IO Int
+cipherGetTagLength = mkGetSize withCipherPtr botan_cipher_get_tag_length
+
+-- /**
+-- * Get the default nonce length of this cipher
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_get_default_nonce_length(botan_cipher_t cipher, size_t* nl);
+foreign import ccall unsafe botan_cipher_get_default_nonce_length :: CipherPtr -> Ptr CSize -> IO BotanErrorCode
+
+cipherGetDefaultNonceLength :: Cipher -> IO Int
+cipherGetDefaultNonceLength = mkGetSize withCipherPtr botan_cipher_get_default_nonce_length
+
+-- /**
+-- * Return the update granularity of the cipher; botan_cipher_update must be
+-- * called with blocks of this size, except for the final.
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_get_update_granularity(botan_cipher_t cipher, size_t* ug);
+foreign import ccall unsafe botan_cipher_get_update_granularity :: CipherPtr -> Ptr CSize -> IO BotanErrorCode
+
+cipherGetUpdateGranularity :: Cipher -> IO Int
+cipherGetUpdateGranularity = mkGetSize withCipherPtr botan_cipher_get_update_granularity
+
+-- /**
+-- * Return the ideal update granularity of the cipher. This is some multiple of the
+-- * update granularity, reflecting possibilities for optimization.
+-- */
+-- BOTAN_PUBLIC_API(3,0) int botan_cipher_get_ideal_update_granularity(botan_cipher_t cipher, size_t* ug);
+foreign import ccall unsafe botan_cipher_get_ideal_update_granularity :: CipherPtr -> Ptr CSize -> IO BotanErrorCode
+
+cipherGetIdealUpdateGranularity :: Cipher -> IO Int
+cipherGetIdealUpdateGranularity = mkGetSize withCipherPtr botan_cipher_get_ideal_update_granularity
+
+-- /**
+-- * Get information about the key lengths. Prefer botan_cipher_get_keyspec
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_query_keylen(botan_cipher_t,
+--                                         size_t* out_minimum_keylength,
+--                                         size_t* out_maximum_keylength);
+foreign import ccall unsafe botan_cipher_query_keylen :: CipherPtr -> Ptr CSize -> Ptr CSize -> IO BotanErrorCode
+
+cipherQueryKeylen :: Cipher -> IO (Int,Int)
+cipherQueryKeylen = mkGetSizes2 withCipherPtr botan_cipher_query_keylen
+
+-- /**
+-- * Get information about the supported key lengths.
+-- */
+-- BOTAN_PUBLIC_API(2,8) int botan_cipher_get_keyspec(botan_cipher_t,
+--                                                    size_t* min_keylen,
+--                                                    size_t* max_keylen,
+--                                                    size_t* mod_keylen);
+foreign import ccall unsafe botan_cipher_get_keyspec :: CipherPtr -> Ptr CSize -> Ptr CSize -> Ptr CSize -> IO BotanErrorCode
+
+cipherGetKeyspec :: Cipher -> IO (Int,Int,Int)
+cipherGetKeyspec = mkGetSizes3 withCipherPtr botan_cipher_get_keyspec
+
+-- /**
+-- * Set the key for this cipher object
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_set_key(botan_cipher_t cipher,
+--                                                const uint8_t* key, size_t key_len);
+foreign import ccall unsafe botan_cipher_set_key :: CipherPtr -> Ptr Word8 -> CSize -> IO BotanErrorCode
+
+cipherSetKey :: Cipher -> ByteString -> IO ()
+cipherSetKey = mkSetBytesLen withCipherPtr botan_cipher_set_key
+
+-- /**
+-- * Reset the message specific state for this cipher.
+-- * Without resetting the keys, this resets the nonce, and any state
+-- * associated with any message bits that have been processed so far.
+-- *
+-- * It is conceptually equivalent to calling botan_cipher_clear followed
+-- * by botan_cipher_set_key with the original key.
+-- */
+-- BOTAN_PUBLIC_API(2,8) int botan_cipher_reset(botan_cipher_t cipher);
+foreign import ccall unsafe botan_cipher_reset :: CipherPtr -> IO BotanErrorCode
+
+cipherReset :: Cipher -> IO ()
+cipherReset = mkAction withCipherPtr botan_cipher_reset
+
+-- /**
+-- * Set the associated data. Will fail if cipher is not an AEAD
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_set_associated_data(botan_cipher_t cipher,
+--                                                const uint8_t* ad, size_t ad_len);
+foreign import ccall unsafe botan_cipher_set_associated_data :: CipherPtr -> Ptr Word8 -> CSize -> IO BotanErrorCode
+
+cipherSetAssociatedData :: Cipher -> ByteString -> IO ()
+cipherSetAssociatedData = mkSetBytesLen withCipherPtr botan_cipher_set_associated_data
+
+-- /**
+-- * Begin processing a new message using the provided nonce
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_start(botan_cipher_t cipher,
+--                                  const uint8_t* nonce, size_t nonce_len);
+foreign import ccall unsafe botan_cipher_start :: CipherPtr -> Ptr Word8 -> CSize -> IO BotanErrorCode
+
+cipherStart :: Cipher -> ByteString -> IO ()
+cipherStart = mkSetBytesLen withCipherPtr botan_cipher_start
+
+-- #define BOTAN_CIPHER_UPDATE_FLAG_FINAL (1U << 0)
+
+type CipherUpdateFlags = Word32
+
+pattern BOTAN_CIPHER_UPDATE_FLAG_NONE = 0 :: CipherUpdateFlags -- NOTE: Not canonical flag
+pattern BOTAN_CIPHER_UPDATE_FLAG_FINAL = 1 :: CipherUpdateFlags
+
+-- /**
+-- * Encrypt some data
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_update(botan_cipher_t cipher,
+--                                   uint32_t flags,
+--                                   uint8_t output[],
+--                                   size_t output_size,
+--                                   size_t* output_written,
+--                                   const uint8_t input_bytes[],
+--                                   size_t input_size,
+--                                   size_t* input_consumed);
+foreign import ccall unsafe botan_cipher_update
+    :: CipherPtr
+    -> CipherUpdateFlags
+    -> Ptr Word8    -- output
+    -> CSize        -- output_size
+    -> Ptr CSize    -- output_written
+    -> Ptr Word8    -- input_bytes
+    -> CSize        -- input_size
+    -> Ptr CSize    -- input_consumed
+    -> IO BotanErrorCode
+
+cipherUpdate :: Cipher -> CipherUpdateFlags-> Int -> ByteString -> IO (Int,ByteString)
+cipherUpdate cipher flags outputSz input = withCipherPtr cipher $ \ cipherPtr -> do
+    asBytesLen input $ \ inputPtr inputSz -> do
+        alloca $ \ consumedPtr -> do
+            alloca $ \ writtenPtr -> do
+                -- Or just allocBytes outputSz $ \ _ -> return (); unsafeAsBytes ...
+                output <- allocBytes outputSz $ \ outputPtr -> do
+                    throwBotanIfNegative_ $ botan_cipher_update
+                        cipherPtr
+                        flags
+                        outputPtr
+                        (fromIntegral outputSz)
+                        writtenPtr
+                        inputPtr
+                        inputSz
+                        consumedPtr
+                consumed <- fromIntegral <$> peek consumedPtr
+                written <- fromIntegral <$> peek writtenPtr
+                return $ (consumed, ByteString.copy $ ByteString.take written output)
+
+-- /**
+-- * Reset the key, nonce, AD and all other state on this cipher object
+-- */
+-- BOTAN_PUBLIC_API(2,0) int botan_cipher_clear(botan_cipher_t hash);
+foreign import ccall unsafe botan_cipher_clear :: CipherPtr -> IO BotanErrorCode
+
+cipherClear :: Cipher -> IO ()
+cipherClear = mkAction withCipherPtr botan_cipher_clear
--- src/Botan/Make.hs
+++ src/Botan/Make.hs
@@ -95,3 +95,120 @@ mkInit_name_flags constr init destroy name flags = do
         foreignPtr <- newForeignPtr destroy out
         return $ constr foreignPtr
 
+{-
+Non-effectful queries
+-}
+
+type GetName ptr = ptr -> Ptr CChar -> Ptr CSize -> IO BotanErrorCode
+
+mkGetName
+    :: WithPtr typ ptr
+    -> GetName ptr
+    -> typ -> IO ByteString
+mkGetName withPtr get typ = withPtr typ $ \ typPtr -> do
+    -- TODO: use ByteString.Internal.createAndTrim?
+    -- NOTE: This uses copy to mimic ByteArray.take (which copies!) so we can drop the rest of the bytestring
+    alloca $ \ szPtr -> do
+        bytes <- allocBytes 64 $ \ bytesPtr -> do
+            throwBotanIfNegative_ $ get typPtr bytesPtr szPtr
+        sz <- peek szPtr
+        return $ ByteString.copy $ ByteString.take (fromIntegral sz) bytes
+
+type GetSize ptr = ptr -> Ptr CSize -> IO BotanErrorCode
+type GetSize_csize ptr = ptr -> CSize -> Ptr CSize -> IO BotanErrorCode
+type GetSizes2 ptr = ptr -> Ptr CSize -> Ptr CSize -> IO BotanErrorCode
+type GetSizes3 ptr = ptr -> Ptr CSize -> Ptr CSize -> Ptr CSize -> IO BotanErrorCode
+
+mkGetSize
+    :: WithPtr typ ptr
+    -> GetSize ptr
+    -> typ -> IO Int
+mkGetSize withPtr get typ = withPtr typ $ \ typPtr -> do
+    alloca $ \ szPtr -> do
+        throwBotanIfNegative_ $ get typPtr szPtr
+        fromIntegral <$> peek szPtr
+
+mkGetSize_csize
+    :: WithPtr typ ptr
+    -> GetSize_csize ptr
+    -> typ -> Int -> IO Int
+mkGetSize_csize withPtr get typ forSz = withPtr typ $ \ typPtr -> do
+    alloca $ \ szPtr -> do
+        throwBotanIfNegative_ $ get typPtr (fromIntegral forSz) szPtr
+        fromIntegral <$> peek szPtr
+
+mkGetSizes2
+    :: WithPtr typ ptr
+    -> GetSizes2 ptr
+    -> typ -> IO (Int,Int)
+mkGetSizes2 withPtr get typ = withPtr typ $ \ typPtr -> do
+    alloca $ \ szPtrA -> alloca $ \ szPtrB -> do
+        throwBotanIfNegative_ $ get typPtr szPtrA szPtrB
+        szA <- fromIntegral <$> peek szPtrA
+        szB <- fromIntegral <$> peek szPtrB
+        return (szA,szB)
+
+mkGetSizes3
+    :: WithPtr typ ptr
+    -> GetSizes3 ptr
+    -> typ -> IO (Int,Int,Int)
+mkGetSizes3 withPtr get typ = withPtr typ $ \ typPtr -> do
+    alloca $ \ szPtrA -> alloca $ \ szPtrB -> alloca $ \ szPtrC -> do
+        throwBotanIfNegative_ $ get typPtr szPtrA szPtrB szPtrC
+        szA <- fromIntegral <$> peek szPtrA
+        szB <- fromIntegral <$> peek szPtrB
+        szC <- fromIntegral <$> peek szPtrC
+        return (szA,szB,szC)
+
+-- type GetBytes ptr = ptr -> Ptr Word8 -> CSize -> IO BotanErrorCode
+
+type GetBoolCode ptr = ptr -> IO BotanErrorCode
+type GetBoolCode_csize ptr = ptr -> CSize -> IO BotanErrorCode
+
+mkGetBoolCode
+    :: WithPtr typ ptr
+    -> GetBoolCode ptr
+    -> typ -> IO Bool
+mkGetBoolCode withPtr get typ = withPtr typ $ \ typPtr -> do
+    throwBotanCatchingBool $ get typPtr
+
+mkGetBoolCode_csize
+    :: WithPtr typ ptr
+    -> GetBoolCode_csize ptr
+    -> typ -> Int -> IO Bool
+mkGetBoolCode_csize withPtr get typ sz = withPtr typ $ \ typPtr -> do
+    throwBotanCatchingBool $ get typPtr (fromIntegral sz)
+
+type GetIntCode ptr = ptr -> IO BotanErrorCode
+type GetIntCode_csize ptr = ptr -> CSize -> IO BotanErrorCode
+
+{-
+Effectful actions
+-}
+
+type Action ptr = ptr -> IO BotanErrorCode
+mkAction
+    :: WithPtr typ ptr
+    -> Action ptr
+    -> typ -> IO ()
+mkAction withPtr action typ = withPtr typ $ \ typPtr -> do
+    throwBotanIfNegative_ $ action typPtr
+
+type SetCString ptr = ptr -> CString -> IO BotanErrorCode
+type SetBytesLen ptr = ptr -> Ptr Word8 -> CSize -> IO BotanErrorCode
+
+mkSetCString
+    :: WithPtr typ ptr
+    -> SetCString ptr
+    -> typ -> ByteString -> IO ()
+mkSetCString withPtr set typ cstring = withPtr typ $ \ typPtr -> do
+    asCString cstring $ \ cstringPtr -> do 
+        throwBotanIfNegative_ $ set typPtr cstringPtr
+
+mkSetBytesLen
+    :: WithPtr typ ptr
+    -> SetBytesLen ptr
+    -> typ -> ByteString -> IO ()
+mkSetBytesLen withPtr set typ bytes = withPtr typ $ \ typPtr -> do
+    asBytesLen bytes $ \ bytesPtr bytesLen -> do 
+        throwBotanIfNegative_ $ set typPtr bytesPtr bytesLen
