ce7a1cbb1036846d5392cca21189cd6b1533c6e9
Leo <leo@apotheca.io>
2023-08-25 10:26:07 -0500

    Added unit test for Bcrypt

--- botan-low/test/Botan/Low/BcryptSpec.hs
+++ botan-low/test/Botan/Low/BcryptSpec.hs
@@ -4,8 +4,23 @@ module Botan.Low.BcryptSpec
 
 import Test.Prelude
 
+import Botan.Low.RNG
+import Botan.Low.Bcrypt
+
+password = "Fee fi fo fum!"
+
+factor = 12
+
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $ do
-            pending
+    describe "bcryptGenerateIO" $ do
+        it "generates a bcrypt hash" $ do
+            rng <- rngCtxInitNameIO "user-threadsafe"
+            _ <- bcryptGenerateIO password rng factor
+            pass
+    describe "bcryptIsValidIO" $ do
+        it "validates a bcrypt hash" $ do
+            rng <- rngCtxInitNameIO "user-threadsafe"
+            h <- bcryptGenerateIO password rng factor
+            valid <- bcryptIsValidIO password h
+            valid `shouldBe` True
--- botan-low/test/Test/Prelude.hs
+++ botan-low/test/Test/Prelude.hs
@@ -1,14 +1,37 @@
 module Test.Prelude
 ( module Prelude
 , module Test.Hspec
+, module Test.QuickCheck
 , module Control.Monad
 , module Data.ByteString
+, chars
+, testSuite
+, pass
 ) where
 
 import Prelude
 
 import Test.Hspec
+import Test.QuickCheck
 
 import Control.Monad
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as Char8
+
+chars :: ByteString -> [Char]
+chars = Char8.unpack
+
+-- describeText = describe . Text.unpack
+
+-- describeBytes :: ByteString -> SpecWith a -> SpecWith a
+-- describeBytes = describe . chars
+
+testSuite :: [t2] -> (t2 -> String) -> (t2 -> SpecWith a) -> SpecWith a
+testSuite tests testName runTest = forM_ tests $ \ test -> describe (testName test) (runTest test)
+
+-- An alternative to void?
+--  _ <- performSomeAction
+--  pass
+pass :: (Monad m) => m ()
+pass = return ()
