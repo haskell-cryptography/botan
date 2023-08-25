33720c291d9d515dd87b9238552cd5db0554abab
Leo <leo@apotheca.io>
2023-08-25 10:27:43 -0500

    Unit test for Botan.Low.ZFEC

--- botan-low/test/Botan/Low/ZFECSpec.hs
+++ botan-low/test/Botan/Low/ZFECSpec.hs
@@ -4,8 +4,26 @@ module Botan.Low.ZFECSpec
 
 import Test.Prelude
 
+import Botan.Low.ZFEC
+
+k :: Int
+k = 5
+
+n :: Int
+n = 7
+
+message :: ByteString
+message = "Fee fi fo fum! I smell the blood of an Englishman!"
+
 spec :: Spec
 spec = do
-    describe "" $ do
-        it "" $ do
-            pending
+    describe "zfecEncodeIO" $ do
+        it "encodes a message into shares" $ do
+            shares <- zfecEncodeIO k n message
+            length shares `shouldBe` n
+    describe "zfecDecodeIO" $ do
+        it "recovers a message from enough shares" $ do
+            shares <- zfecEncodeIO k n message
+            someShares <- take k <$> generate (shuffle shares)
+            recoveredMessage <- zfecDecodeIO k n someShares
+            recoveredMessage `shouldBe` message
