c7c88abeb5df0d18b64790a4b8f91c64ec038556
Leo <leo@apotheca.io>
2023-11-27 12:49:41 -0600

    Botan.Low.X509.CSR implementations

--- botan-low/src/Botan/Low/X509/CSR.hs
+++ botan-low/src/Botan/Low/X509/CSR.hs
@@ -2,6 +2,7 @@ module Botan.Low.X509.CSR where
 
 import Botan.Low.Error
 import Botan.Low.Hash (HashName(..))
+import Botan.Low.Make
 import Botan.Low.Prelude
 import Botan.Low.PubKey
 import Botan.Low.RNG
@@ -9,6 +10,7 @@ import Botan.Low.X509
 import Botan.Low.X509.Extensions
 import Botan.Low.X509.Options
 
+import Botan.Bindings.X509
 import Botan.Bindings.X509.CSR
 
 -- /*
@@ -30,38 +32,54 @@ withX509CSRPtr = withForeignPtr . getX509CSRForeignPtr
 x509CSRDestroy :: X509CSR -> IO ()
 x509CSRDestroy csr = finalizeForeignPtr (getX509CSRForeignPtr csr)
 
+-- NOTE: It really appears that this is the successful init pattern;
+--  I ought to formalize it using type-level lists or something
 x509CreateCertReq :: X509CertOptions -> PrivKey -> HashName -> RNGCtx -> IO X509CSR
-x509CreateCertReq = undefined
+x509CreateCertReq options key hash_fn rng = withX509CertOptionsPtr options $ \ optionsPtr -> do
+    withPrivKeyPtr key $ \ keyPtr -> do
+        asCString hash_fn $ \ hashPtr -> do
+            withRNGPtr rng $ \ rngPtr -> do
+                mkInit
+                    MkX509CSR
+                    (\ csrPtr -> botan_x509_create_cert_req csrPtr optionsPtr keyPtr hashPtr rngPtr)
+                    botan_x509_csr_destroy
 
 x509CSRCreate :: PrivKey -> X509SubjectDN -> X509Extensions -> HashName -> RNGCtx -> X509PaddingName -> X509Challenge -> IO X509CSR
-x509CSRCreate privKey subjectDn exts hash_fn rng padding_fn challenge = undefined
+x509CSRCreate privkey subjectDN exts hash_fn rng padding_fn challenge = do
+    withPrivKeyPtr privkey $ \ privkeyPtr -> do
+        asBytesLen subjectDN $ \ subjectDNPtr subjectDNLen -> do
+            withX509ExtensionsPtr exts $ \ extsPtr -> do
+                asCString hash_fn $ \ hashPtr -> do
+                    withRNGPtr rng $ \ rngPtr -> do
+                        asCString padding_fn $ \ paddingPtr -> do
+                            asCString challenge $ \ challengePtr -> do
+                                mkInit
+                                    MkX509CSR
+                                    (\ csrPtr -> botan_x509_csr_create
+                                        csrPtr
+                                        privkeyPtr
+                                        subjectDNPtr subjectDNLen
+                                        extsPtr
+                                        hashPtr
+                                        rngPtr
+                                        paddingPtr
+                                        challengePtr
+                                    )
+                                    botan_x509_csr_destroy
 
 x509CreateSelfSignedCert :: X509CertOptions -> PrivKey -> HashName -> RNGCtx -> IO X509Cert
-x509CreateSelfSignedCert = undefined
-
-
--- -- Certificate Signing Request, rename to that / CSR at higher levels
--- data X509CSRStruct
--- type X509CSRPtr = Ptr X509CSRStruct
-
--- foreign import ccall unsafe "&botan_x509_csr_destroy" botan_x509_csr_destroy :: FinalizerPtr X509CSRStruct
-
--- foreign import ccall unsafe botan_x509_create_cert_req
---     :: Ptr X509CSRPtr
---     -> X509CertOptionsPtr
---     -> PrivKeyPtr
---     -> Ptr CChar
---     -> RNGPtr
---     -> IO BotanErrorCode
-
--- foreign import ccall unsafe botan_x509_csr_create
---     :: Ptr X509CSRPtr
---     -> PrivKeyPtr
---     -> Ptr Word8 -> CSize
---     -> X509ExtensionsPtr
---     -> Ptr CChar
---     -> RNGPtr
---     -> Ptr CChar
---     -> Ptr CChar
---     -> IO BotanErrorCode
-    
+x509CreateSelfSignedCert options key hash_fn rng = do
+    withX509CertOptionsPtr options $ \ optionsPtr -> do
+        withPrivKeyPtr key $ \ keyPtr -> do
+            asCString hash_fn $ \ hashPtr -> do
+                withRNGPtr rng $ \ rngPtr -> do
+                    mkInit
+                        MkX509Cert
+                        (\ certPtr -> botan_x509_create_self_signed_cert
+                            certPtr
+                            optionsPtr
+                            keyPtr
+                            hashPtr
+                            rngPtr
+                        )
+                        botan_x509_cert_destroy
