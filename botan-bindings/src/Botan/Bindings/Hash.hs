ad75cdb6f499855804d4d8df25357d095e82e478
Leo <leo@apotheca.io>
2023-12-20 11:04:32 -0600

    Converted Botan.Bindings.Hash to CAPI

--- botan-bindings/src/Botan/Bindings/Hash.hs
+++ botan-bindings/src/Botan/Bindings/Hash.hs
@@ -25,117 +25,91 @@ the arguments concatenated. After completing a hash computation (eg using
 hashFinal), the internal state is reset to begin hashing a new message.
 -}
 
+{-# LANGUAGE CApiFFI #-}
+
 module Botan.Bindings.Hash where
 
-import Botan.Bindings.Error
 import Botan.Bindings.Prelude
 
-{-|
-Hash type
+-- | Opaque Hash struct
+data {-# CTYPE "botan/ffi.h" "struct botan_hash_struct" #-} BotanHashStruct
 
-@typedef struct botan_hash_struct* botan_hash_t;@
--}
+-- | Botan Hash object
+newtype {-# CTYPE "botan/ffi.h" "botan_hash_t" #-} BotanHash
+    = MkBotanHash { runBotanHash :: Ptr BotanHashStruct }
+        deriving newtype (Eq, Ord, Storable)
 
-data HashStruct
-type HashPtr = Ptr HashStruct
+-- | Frees all resources of the hash object
+foreign import capi safe "botan/ffi.h &botan_hash_destroy"
+    botan_hash_destroy
+        :: FinalizerPtr BotanHashStruct
 
-{-|
-Initialize a hash function object
+-- WARNING: Remove after fixing botan-low
+type HashPtr = BotanHash
+type HashStruct = BotanHashStruct
 
-- \@param hash hash object
-- \@param hash_name name of the hash function, e.g., "SHA-384"
-- \@param flags should be 0 in current API revision, all other uses are reserved
-      and return BOTAN_FFI_ERROR_BAD_FLAG
+-- | Initialize a hash function object
+foreign import capi safe "botan/ffi.h botan_hash_init"
+    botan_hash_init
+        :: Ptr BotanHash    -- ^ hash hash object
+        -> ConstPtr CChar   -- ^ hash_name name of the hash function, e.g., "SHA-384"
+        -> Word32           -- ^ flags should be 0 in current API revision, all other uses are reserved
+                            --   and return BOTAN_FFI_ERROR_BAD_FLAG
+        -> IO CInt      
 
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_init(botan_hash_t* hash, const char* hash_name, uint32_t flags);@
--}
-foreign import ccall unsafe botan_hash_init :: Ptr HashPtr -> Ptr CChar -> Word32 -> IO BotanErrorCode
+-- | Get the name of this hash function
+foreign import capi safe "botan/ffi.h botan_hash_name"
+    botan_hash_name
+        :: BotanHash    -- ^ hash the object to read
+        -> Ptr CChar    -- ^ name output buffer
+        -> Ptr CSize    -- ^ name_len on input, the length of buffer, on success the number of bytes written
+        -> IO CInt
 
-{-|
-Frees all resources of the hash object
+-- | Copy the state of a hash function object
+foreign import capi safe "botan/ffi.h botan_hash_copy_state"
+    botan_hash_copy_state
+        :: Ptr BotanHash    -- ^ dest destination hash object
+        -> BotanHash        -- ^ source source hash object
+        -> IO CInt          -- ^ 0 on success, a negative value on failure
 
-- \@param hash hash object
-- \@return 0 if success, error if invalid object handle
+-- | Writes the output length of the hash function to *output_length
+foreign import capi safe "botan/ffi.h botan_hash_output_length"
+    botan_hash_output_length
+        :: BotanHash    -- ^ hash hash object
+        -> Ptr CSize    -- ^ output_length output buffer to hold the hash function output length
+        -> IO CInt      -- ^ 0 on success, a negative value on failure
 
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_destroy(botan_hash_t hash);@
--}
-foreign import ccall unsafe "&botan_hash_destroy" botan_hash_destroy :: FinalizerPtr HashStruct
+-- | Writes the block size of the hash function to *block_size
+foreign import capi safe "botan/ffi.h botan_hash_block_size"
+    botan_hash_block_size
+        :: BotanHash    -- ^ hash hash object
+        -> Ptr CSize    -- ^ block_size output buffer to hold the hash function output length
+        -> IO CInt      -- ^ 0 on success, a negative value on failure
 
-{-|
-Get the name of this hash function
+-- | Send more input to the hash function
+foreign import capi safe "botan/ffi.h botan_hash_update"
+    botan_hash_update
+        :: BotanHash        -- ^ hash hash object
+        -> ConstPtr Word8   -- ^ in input buffer
+        -> CSize            -- ^ in_len number of bytes to read from the input buffer
+        -> IO CInt          -- ^ 0 on success, a negative value on failure
 
-- \@param hash the object to read
-- \@param name output buffer
-- \@param name_len on input, the length of buffer, on success the number of bytes written
-
-@BOTAN_FFI_EXPORT(2, 8) int botan_hash_name(botan_hash_t hash, char* name, size_t* name_len);@
--}
-foreign import ccall unsafe botan_hash_name :: HashPtr -> Ptr CChar -> Ptr CSize -> IO BotanErrorCode
-
-{-|
-Copy the state of a hash function object
-
-- \@param dest destination hash object
-- \@param source source hash object
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 2) int botan_hash_copy_state(botan_hash_t* dest, const botan_hash_t source);@
--}
-foreign import ccall unsafe botan_hash_copy_state :: Ptr HashPtr -> HashPtr -> IO BotanErrorCode
-
-{-|
-Reinitializes the state of the hash computation. A hash can
-be computed (with update/final) immediately.
-
-- \@param hash hash object
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_clear(botan_hash_t hash);@
--}
-foreign import ccall unsafe botan_hash_clear :: HashPtr -> IO BotanErrorCode
-
-{-|
-Writes the block size of the hash function to *block_size
-
-- \@param hash hash object
-- \@param block_size output buffer to hold the hash function output length
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 2) int botan_hash_block_size(botan_hash_t hash, size_t* block_size);@
--}
-foreign import ccall unsafe botan_hash_block_size :: HashPtr -> Ptr CSize -> IO BotanErrorCode
-
-{-|
-Writes the output length of the hash function to *output_length
-- \@param hash hash object
-- \@param output_length output buffer to hold the hash function output length
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_output_length(botan_hash_t hash, size_t* output_length);@
--}
-foreign import ccall unsafe botan_hash_output_length :: HashPtr -> Ptr CSize -> IO BotanErrorCode
-
-{-|
-Send more input to the hash function
-
-- \@param hash hash object
-- \@param in input buffer
-- \@param in_len number of bytes to read from the input buffer
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_update(botan_hash_t hash, const uint8_t* in, size_t in_len);@
--}
-foreign import ccall unsafe botan_hash_update :: HashPtr -> Ptr Word8  -> CSize -> IO BotanErrorCode
-
-{-|
+{- |
 Finalizes the hash computation and writes the output to
 out[0:botan_hash_output_length()] then reinitializes for computing
 another digest as if botan_hash_clear had been called.
-      
-- \@param hash hash object
-- \@param out output buffer
-- \@return 0 on success, a negative value on failure
-
-@BOTAN_FFI_EXPORT(2, 0) int botan_hash_final(botan_hash_t hash, uint8_t out[]);@
 -}
-foreign import ccall unsafe botan_hash_final :: HashPtr -> Ptr Word8 -> IO BotanErrorCode
+foreign import capi safe "botan/ffi.h botan_hash_final"
+    botan_hash_final
+        :: BotanHash    -- ^ hash hash object
+        -> Ptr Word8    -- ^ out[] output buffer
+        -> IO CInt      -- ^ 0 on success, a negative value on failure
+
+{- |
+Reinitializes the state of the hash computation. A hash can
+be computed (with update/final) immediately.
+-}
+foreign import capi safe "botan/ffi.h botan_hash_clear"
+    botan_hash_clear
+        :: BotanHash    -- ^ hash hash object
+        -> IO CInt      -- 0 on success, a negative value on failure
