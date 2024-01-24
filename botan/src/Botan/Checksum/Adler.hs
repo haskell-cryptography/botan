53c2fbdaa2a5f6f343a642bc703db7fa095ed5eb
Leo <leo@apotheca.io>
2024-01-24 15:48:48 -0600

    Added Checksums Adler32 CRC24 CRC32

---
+++ botan/src/Botan/Checksum/Adler.hs
@@ -0,0 +1,46 @@
+module Botan.Checksum.Adler
+( Adler32(..)
+, Adler32Digest(..)
+, adler32
+, adler32Lazy
+) where
+
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as Lazy
+import qualified Data.Text as Text
+
+import qualified Botan.Hash as Botan
+import qualified Botan.Utility as Botan
+
+import Botan.Hash.Class -- Crypto.Hash
+import Botan.Prelude
+
+-- Adler32 type
+
+data Adler32
+
+newtype instance Digest Adler32 = Adler32Digest
+    { getAdler32ByteString :: ByteString {- ByteVector n -} }
+    deriving newtype (Eq, Ord)
+
+type Adler32Digest = Digest Adler32
+
+instance Show (Digest Adler32) where
+    show :: Digest Adler32 -> String
+    show (Adler32Digest bytes) = Text.unpack $ Botan.hexEncode bytes Botan.Lower
+
+instance Hash Adler32 where
+    hash :: ByteString -> Digest Adler32
+    hash = Adler32Digest . Botan.hash Botan.adler32
+
+instance IncrementalHash Adler32 where
+    hashLazy :: Lazy.ByteString -> Digest Adler32
+    hashLazy = Adler32Digest . Botan.hashLazy Botan.adler32
+
+-- Adler32 hash
+
+adler32 :: ByteString -> Adler32Digest
+adler32 = hash
+
+adler32Lazy :: Lazy.ByteString -> Adler32Digest
+adler32Lazy = hashLazy
---
+++ botan/src/Botan/Checksum/CRC.hs
@@ -0,0 +1,81 @@
+module Botan.Checksum.CRC
+( CRC24(..)
+, CRC24Digest(..)
+, crc24
+, crc24Lazy
+, CRC32(..)
+, CRC32Digest(..)
+, crc32
+, crc32Lazy
+) where
+
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as Lazy
+import qualified Data.Text as Text
+
+import qualified Botan.Hash as Botan
+import qualified Botan.Utility as Botan
+
+import Botan.Hash.Class -- Crypto.Hash
+import Botan.Prelude
+
+-- CRC24 type
+
+data CRC24
+
+newtype instance Digest CRC24 = CRC24Digest
+    { getCRC24ByteString :: ByteString {- ByteVector n -} }
+    deriving newtype (Eq, Ord)
+
+type CRC24Digest = Digest CRC24
+
+instance Show (Digest CRC24) where
+    show :: Digest CRC24 -> String
+    show (CRC24Digest bytes) = Text.unpack $ Botan.hexEncode bytes Botan.Lower
+
+instance Hash CRC24 where
+    hash :: ByteString -> Digest CRC24
+    hash = CRC24Digest . Botan.hash Botan.crc24
+
+instance IncrementalHash CRC24 where
+    hashLazy :: Lazy.ByteString -> Digest CRC24
+    hashLazy = CRC24Digest . Botan.hashLazy Botan.crc24
+
+-- CRC24 hash
+
+crc24 :: ByteString -> CRC24Digest
+crc24 = hash
+
+crc24Lazy :: Lazy.ByteString -> CRC24Digest
+crc24Lazy = hashLazy
+
+-- CRC32 type
+
+data CRC32
+
+newtype instance Digest CRC32 = CRC32Digest
+    { getCRC32ByteString :: ByteString {- ByteVector n -} }
+    deriving newtype (Eq, Ord)
+
+type CRC32Digest = Digest CRC32
+
+instance Show (Digest CRC32) where
+    show :: Digest CRC32 -> String
+    show (CRC32Digest bytes) = Text.unpack $ Botan.hexEncode bytes Botan.Lower
+
+instance Hash CRC32 where
+    hash :: ByteString -> Digest CRC32
+    hash = CRC32Digest . Botan.hash Botan.crc32
+
+instance IncrementalHash CRC32 where
+    hashLazy :: Lazy.ByteString -> Digest CRC32
+    hashLazy = CRC32Digest . Botan.hashLazy Botan.crc32
+
+-- CRC32 hash
+
+crc32 :: ByteString -> CRC32Digest
+crc32 = hash
+
+crc32Lazy :: Lazy.ByteString -> CRC32Digest
+crc32Lazy = hashLazy
+
