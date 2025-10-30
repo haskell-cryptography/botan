3cf8781cdcc7bad48e24e1acf3ffd1b89a51f106
Joris Dral <joris@well-typed.com>
2025-10-30 17:00:13 +0100

    Move `botan-low-hotp-tests` into `botan-low:test` component

--- botan-low/test/Main.hs
+++ botan-low/test/Main.hs
@@ -4,6 +4,7 @@ import qualified Test.Botan.Low.Bcrypt
 import qualified Test.Botan.Low.BlockCipher
 import qualified Test.Botan.Low.FPE
 import qualified Test.Botan.Low.Hash
+import qualified Test.Botan.Low.HOTP
 import qualified Test.Botan.Low.PwdHash
 import qualified Test.Botan.Low.SRP6
 import qualified Test.Botan.Low.SRP6.Example
@@ -19,6 +20,7 @@ tests = do
     blockCipherTests <- Test.Botan.Low.BlockCipher.tests
     fpeTests <- Test.Botan.Low.FPE.tests
     hashTests <- Test.Botan.Low.Hash.tests
+    hotpTests <- Test.Botan.Low.HOTP.tests
     pwdHashTests <- Test.Botan.Low.PwdHash.tests
     srp6Tests <- Test.Botan.Low.SRP6.tests
     pure $ testGroup "botan-low" [
@@ -26,6 +28,7 @@ tests = do
       , blockCipherTests
       , fpeTests
       , hashTests
+      , hotpTests
       , pwdHashTests
       , srp6Tests
       , testGroup "Test.Botan.Low.SRP6.Example" [
--- botan-low/test/Test/Botan/Low/HOTP.hs
+++ botan-low/test/Test/Botan/Low/HOTP.hs
@@ -1,8 +1,21 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import           Test.Prelude
+module Test.Botan.Low.HOTP (tests) where
 
 import           Botan.Low.HOTP
+import           Data.ByteString
+import           Test.Hspec
+import           Test.Tasty
+import           Test.Tasty.Hspec
+import           Test.Util.ByteString
+import           Test.Util.Hspec
+
+tests :: IO TestTree
+tests = do
+    specs <- testSpec "spec_HOTP" spec_HOTP
+    pure $ testGroup "Test.Botan.Low.HOTP" [
+        specs
+      ]
 
 key :: ByteString
 key = "Fee fi fo fum"
@@ -10,8 +23,8 @@ key = "Fee fi fo fum"
 counter :: HOTPCounter
 counter = 12345
 
-main :: IO ()
-main = hspec $ testSuite hotpHashes chars $ \ h -> do
+spec_HOTP :: Spec
+spec_HOTP = testSuite hotpHashes chars $ \ h -> do
     it "hotpInit" $ do
         _ctx <- hotpInit key h 6
         pass
