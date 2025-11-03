74d76ac8e34e093d86a4c15b7f5b984b0303c479
Joris Dral <joris@well-typed.com>
2025-11-03 17:40:33 +0100

    `Utility`: format the module

--- botan-low/src/Botan/Low/Utility.hs
+++ botan-low/src/Botan/Low/Utility.hs
@@ -25,97 +25,92 @@ module Botan.Low.Utility (
   , base64Decode
   ) where
 
-import           Data.Bool
-import qualified Data.Text.Encoding as Text
-
 import           Botan.Bindings.Utility
-
 import           Botan.Low.Error
 import           Botan.Low.Make (allocBytesQuerying, allocBytesQueryingCString)
 import           Botan.Low.Prelude
+import qualified Data.Text.Encoding as Text
 
 -- | Returns 0 if x[0..len] == y[0..len], -1 otherwise.
-constantTimeCompare
-    :: ByteString   -- ^ __x__
-    -> ByteString   -- ^ __y__
-    -> Int          -- ^ __len__
-    -> IO Bool
-constantTimeCompare x y len = do
-    asBytesLen x $ \ xPtr _xlen -> do
-        asBytesLen y $ \ yPtr _ylen -> do
-            result <- botan_constant_time_compare
-                (ConstPtr xPtr)
-                (ConstPtr yPtr)
-                (fromIntegral len)
-            case result of
-                0 -> return True
-                _ -> return False
+constantTimeCompare ::
+     ByteString   -- ^ __x__
+  -> ByteString   -- ^ __y__
+  -> Int          -- ^ __len__
+  -> IO Bool
+constantTimeCompare x y len =
+    asBytesLen x $ \ xPtr _xlen ->
+    asBytesLen y $ \ yPtr _ylen -> do
+      result <- botan_constant_time_compare
+        (ConstPtr xPtr)
+        (ConstPtr yPtr)
+        (fromIntegral len)
+      case result of
+        0 -> return True
+        _ -> return False
 
-scrubMem
-    :: Ptr a    -- ^ __mem__
-    -> Int      -- ^ __bytes__
-    -> IO ()
+scrubMem ::
+     Ptr a    -- ^ __mem__
+  -> Int      -- ^ __bytes__
+  -> IO ()
 scrubMem ptr sz = throwBotanIfNegative_ $ botan_scrub_mem (castPtr ptr) (fromIntegral sz)
 
 type HexEncodingFlags = Word32
 
-pattern HexUpperCase    -- ^ NOTE: Not an actual flag
-    ,   HexLowerCase
-    ::  HexEncodingFlags
+pattern
+    HexUpperCase
+  , HexLowerCase
+  :: HexEncodingFlags
 
 pattern HexUpperCase = BOTAN_FFI_HEX_UPPER_CASE
 pattern HexLowerCase = BOTAN_FFI_HEX_LOWER_CASE
 
--- | Performs hex encoding of binary data in x of size len bytes. The output
--- buffer out must be of at least x*2 bytes in size. If flags contains
--- BOTAN_FFI_HEX_LOWER_CASE, hex encoding will only contain lower-case letters,
--- upper-case letters otherwise. Returns 0 on success, 1 otherwise.
-hexEncode
-    :: ByteString           -- ^ __x__
-    -> HexEncodingFlags     -- ^ __flags__
-    -> IO Text              -- ^ __y__
-hexEncode bytes flags =  Text.decodeUtf8 <$> do
-    asBytesLen bytes $ \ bytesPtr bytesLen -> do
-        allocBytes (fromIntegral $ 2 * bytesLen) $ \ hexPtr -> do
-            throwBotanIfNegative_ $ botan_hex_encode
-                (ConstPtr bytesPtr)
-                bytesLen
-                hexPtr
-                flags
+hexEncode ::
+     ByteString           -- ^ __x__
+  -> HexEncodingFlags     -- ^ __flags__
+  -> IO Text              -- ^ __y__
+hexEncode bytes flags =
+    fmap Text.decodeUtf8 $
+    asBytesLen bytes $ \ bytesPtr bytesLen ->
+    allocBytes (fromIntegral $ 2 * bytesLen) $ \ hexPtr ->
+    throwBotanIfNegative_ $ botan_hex_encode
+      (ConstPtr bytesPtr)
+      bytesLen
+      hexPtr
+      flags
 
--- | Hex decode some data
-hexDecode
-    :: Text             -- ^ __hex_str__
-    -> IO ByteString    -- ^ __out__
-hexDecode txt = do
-    asBytesLen (Text.encodeUtf8 txt) $ \ hexPtr hexLen -> do
-        allocBytesQuerying $ \ bytesPtr szPtr -> do
-            botan_hex_decode
-                (ConstPtr hexPtr)
-                hexLen
-                bytesPtr
-                szPtr
+hexDecode ::
+     Text             -- ^ __hex_str__
+  -> IO ByteString    -- ^ __out__
+hexDecode txt =
+    asBytesLen (Text.encodeUtf8 txt) $ \ hexPtr hexLen ->
+    allocBytesQuerying $ \ bytesPtr szPtr ->
+    botan_hex_decode
+      (ConstPtr hexPtr)
+      hexLen
+      bytesPtr
+      szPtr
 
-base64Encode
-    :: ByteString   -- ^ __x__
-    -> IO Text      -- ^ __out__
-base64Encode bytes = Text.decodeUtf8 <$> do
-    asBytesLen bytes $ \ bytesPtr bytesLen -> do
-        allocBytesQueryingCString $ \ base64Ptr szPtr -> do
-            botan_base64_encode
-                (ConstPtr bytesPtr)
-                bytesLen
-                base64Ptr
-                szPtr
+base64Encode ::
+     ByteString   -- ^ __x__
+  -> IO Text      -- ^ __out__
+base64Encode bytes =
+    fmap Text.decodeUtf8 $
+    asBytesLen bytes $ \ bytesPtr bytesLen ->
+    allocBytesQueryingCString $ \ base64Ptr szPtr ->
+    botan_base64_encode
+      (ConstPtr bytesPtr)
+      bytesLen
+      base64Ptr
+      szPtr
 
-base64Decode
-    :: Text             -- ^ __base64_str__
-    -> IO ByteString    -- ^ __out__
-base64Decode txt = do
-    asBytesLen (Text.encodeUtf8 txt) $ \ base64Ptr base64Len -> do
-        allocBytesQueryingCString $ \ bytesPtr szPtr -> do
-            botan_base64_decode
-                (ConstPtr base64Ptr)
-                base64Len
-                bytesPtr
-                szPtr
+base64Decode ::
+     Text             -- ^ __base64_str__
+  -> IO ByteString    -- ^ __out__
+base64Decode txt =
+    asBytesLen (Text.encodeUtf8 txt) $ \ base64Ptr base64Len ->
+    allocBytesQueryingCString $ \ bytesPtr szPtr ->
+    botan_base64_decode
+      (ConstPtr base64Ptr)
+      base64Len
+      bytesPtr
+      szPtr
